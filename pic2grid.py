ad24cca13f6bb4a55a638ad7aea82d23711e7b5d
Blake Romrell <blakepromrell@gmail.com>
2024-03-20 12:03:28 -0600

    midpoints checked, commented out helper code

--- pic2grid.py
+++ pic2grid.py
@@ -2,23 +2,23 @@ import cv2 as cv
 import numpy as np
 import os
 
-# Load the image
-img = cv.imread("obstacles_image0.jpg")
+# # Load the image
+# img = cv.imread("obstacles_image0.jpg")
 
-cv.imshow("Original Image", img)
-cv.waitKey(0)
+# cv.imshow("Original Image", img)
+# cv.waitKey(0)
 
-imsize = img.shape
-print("Image Size: ", imsize)
+# imsize = img.shape
+# print("Image Size: ", imsize)
 
 
 def crop_down(image, crop_height):
     return image[crop_height:, :, :]
 
 
-cropped = crop_down(img, 120)
-cv.imshow("Cropped Image", cropped)
-cv.waitKey(0)
+# cropped = crop_down(img, 120)
+# cv.imshow("Cropped Image", cropped)
+# cv.waitKey(0)
 
 
 def make_grid(image, n_rows, n_cols, p_occup):
@@ -37,20 +37,26 @@ def make_grid(image, n_rows, n_cols, p_occup):
     return grid
 
 
-grid = make_grid(cropped, 10, 10, 0.33)
-grid_big = cv.resize(
-    grid, None, fx=imsize[1] // 10, fy=imsize[0] // 10, interpolation=cv.INTER_NEAREST
-)
-grid_big *= 250
-cv.imshow("Grid", grid_big)
-cv.waitKey(0)
+# grid = make_grid(cropped, 10, 10, 0.33)
+# grid_big = cv.resize(
+#     grid,
+#     None,
+#     fx=cropped.shape[1] // 10,
+#     fy=cropped.shape[0] // 10,
+#     interpolation=cv.INTER_NEAREST,
+# )
+# grid_big *= 250
+# cv.imshow("Grid", grid_big)
+# cv.waitKey(0)
 
 
 def grid2midpoints(grid):
     midpoints = []
     for i in range(grid.shape[0]):  # rows
         jl = grid.shape[1] // 2 - 1
-        jr = grid.shape[1] // 2
+        while jl >= 0 and grid[i, jl] == 1:
+            jl -= 1
+        jr = jl + 1
         while jl >= 0 and grid[i, jl] == 0:
             jl -= 1
         while jr < grid.shape[1] and grid[i, jr] == 0:
@@ -59,3 +65,21 @@ def grid2midpoints(grid):
         tempr = float(jr)
         midpoints.append((i, (templ + tempr) / 2))
     return midpoints
+
+
+# midpoints = grid2midpoints(grid)
+# for i in range(len(midpoints)):
+#     cv.circle(
+#         grid_big,
+#         (
+#             int(midpoints[i][1] * (grid_big.shape[1] // 10)),
+#             int(midpoints[i][0] * (grid_big.shape[0] // 10)),
+#         ),
+#         5,
+#         (255, 255, 255),
+#         -1,
+#     )
+
+# cv.imshow("Midpoints", grid_big)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
