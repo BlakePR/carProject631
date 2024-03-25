667c88603de3c6bda00a1718335d5eed3fa6169f
blayer0105 <70558222+blayer0105@users.noreply.github.com>
2024-03-25 12:45:47 -0600

    Update ECEnRacer.py

--- ECEnRacer.py
+++ ECEnRacer.py
@@ -26,11 +26,11 @@ from RealSense import *
 import numpy as np
 import imutils
 import cv2
-import pyximport; pyximport.install()
 
 from pic2grid import crop_down, crop_up, make_grid, grid2midpoints
 from pathplanner import find_ave_angle
-import image_processing
+from image_processing import get_obstacle
+
 
 rs = RealSense("/dev/video2", RS_VGA)  # RS_VGA, RS_720P, or RS_1080P
 writer = None
@@ -48,15 +48,16 @@ while True:
     # cv2.imshow("RGB", rgb)
     # cv2.imshow("Depth", depth)
 
-    Car.zero(1562)  # Set car to go straight.  Change this for your car.
+    Car.zero(1576)  # Set car to go straight.  Change this for your car.
 
     """
     Add your code to process rgb, depth, IMU data
     """
     crop = crop_down(rgb, 120)
     crop = crop_up(crop, 30)
-    crop = image_processing.get_obstacle(crop)
-    gridx, gridy = 15, 16
+    crop = cv.resize(crop, (0,0), fx=0.3, fy=0.5)
+    crop = get_obstacle(crop)
+    gridx, gridy = 10, 10
     grid = make_grid(crop, gridx, gridy, 0.35)
     scalex = crop.shape[1] // gridx
     scaley = crop.shape[0] // gridy
@@ -72,10 +73,10 @@ while True:
     else:
         Car.drive(1.3)
     print(angle)
-    # if count % 13 == 0:
-        # cv2.imwrite(f"run2_{count}_crop.jpg", crop)
-        # cv2.imwrite(f"run2_{count}.jpg", rgb)
-    if count > 160:
+    if count % 13 == 0:
+        cv2.imwrite(f"run2_{count}_crop.jpg", crop)
+        cv2.imwrite(f"run2_{count}.jpg", rgb)
+    if count > 40:
         break
     """
    	IMPORTANT: Never go full speed. Use CarTest.py to selest the best speed for you.
